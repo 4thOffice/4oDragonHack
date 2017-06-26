ea298614fa0fdfe40545c9b8a259f2091e9edcef
kajaj <kaja@marg.si>
2017-06-26 16:05:23 +0200

    update structure

---
+++ examples/config.py
@@ -0,0 +1,3 @@
+API_URL = 'https://clean-sprint-app.4thoffice.com/api'
+INTEGRATION_KEY = '1cd65bd6-fd85-4a80-8e1c-3263c0793bdf@4thoffice.com'
+INTEGRATION_SECRET = '31b38f06-50f8-3c0f-92c2-036bbac2fbb6'
